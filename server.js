f54386672bc721b01f9c674baab92eb46d0bfba1
torreseam <615694479+torreseam@users_noreply.github.com>
2020-05-29 22:50:09 -0500

    update get and post request

--- server.js
+++ server.js
@@ -6,9 +6,11 @@ const htmlRoutes = require('./routes/htmlRoutes');
 const path = require('path');
 const express = require('express');
 const fs = require('fs');
+const { notes } = require('./Develop/db/db.json');
+// const { notes } = require('./Develop/db/db.json');
 
 //express - initialize and create
-const Port = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3001;
 const app = express();
 
 // parse incoming string or array data
@@ -20,27 +22,50 @@ app.use('/api', apiRoutes);
 app.use('/', htmlRoutes);
 
 
-function findById(id, notesArray) {
-    const result = notesArray.filter(notes => notes.id === id)[0];
+function findById(id, noteArray) {
+    const result = noteArray.filter(note => note.id === id)[0];
     return result;
 }
 
-// function createNewAnimal(body, animalsArray) {
-//     //body is coming from req.body app.post
-//     const animal = body;
-//     //main function below
-//     animalsArray.push(animal);
+function createMoreNotes(body, notesArray) {
+    //body is coming from req.body app.post
+    const notes = body;
+    //main function below
+    notesArray.push(notes);
 
-//     fs.writeFileSync(
-//         path.join(__dirname, '/data/animals.json'),
-//         JSON.stringify({ animals: animalsArray }, null, 2)
-//     );
+    fs.writeFileSync(
+        path.join(__dirname, '/Develop/db/ db.json'),
+        JSON.stringify({ notes: notesArray }, null, 2)
+    );
 
-//     // return finished code to post route for response
-//     return animal;
-// }
+    // return finished code to post route for response
+    return note;
+}
 
+//establish routes 
+app.get('/api/notes', (req, res) => {
+    let results = notes;
+//     if (req.query) {
+//         results = filterByQuery(req.query, results);
+//     }
+    res.json(results);
+});
 
+app.get('/api/notes/:id', (req, res) => {
+    const result = findById(req.params.id, notes);
+    if (result) {
+        res.json(result);
+    } else {
+        res.sendStatus(404);
+    }
+});
+
+app.post('/api/notes', (req, res) => {
+    // set id based on what the next index of array
+    req.body.id = notes.length.toString();
+    const notes = createMoreNotes(req.body, notes);
+    res.json(req.body);
+});
 
 
 
